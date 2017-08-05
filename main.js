11a7c554727bf870d0bce338a7d2faec372b75d0
Nedrith <nedriths@gmail.com>
2017-08-05 10:27:29 -0400

    Disabled main's spawn code, it is now completely handled by prototype.spawn

--- main.js
+++ main.js
@@ -42,6 +42,7 @@ module.exports.loop = function() {
       delete Memory.creeps[name];
     }
   }
+  /*
   var tower = Game.getObjectById('2fa9c063e5baba3');
 if(tower) {
     var closestHostile = tower.pos.findClosestByRange(FIND_HOSTILE_CREEPS);
@@ -58,7 +59,7 @@ if(tower) {
       tower.attack(closestHostile);
   }
 }
-
+*/
 
 
   //console.log(debug)
@@ -83,6 +84,7 @@ if(tower) {
     //every time debug is turned on update container locations
     if(debug)
     {
+
       spawn.room.updateSourceContainers();
       spawn.room.updateControllerContainer();
     }
@@ -92,59 +94,39 @@ if(tower) {
       spawn.spawnCheck(debug)
     }
   }
+  /*
   for(let n in Game.spawns)
   {
     var spawn = Game.spawns[n];
     var creepsInRoom = spawn.room.find(FIND_CREEPS);
 
-  //console.log(spawn.name);
   var energyMax = Game.spawns.Spawn1.room.energyCapacityAvailable - 200;
   if (energyMax > 1000)
     energy = 1000;
   else
     energy = energyMax;
-  //var energy = 400;
+
   var numberOfHarvesters = _.sum(creepsInRoom, (c) => c.memory.role == 'harvester');
-  //if(debug == true){console.log(spawn.name + ' harvesters: ' + numberOfHarvesters);}
-//console.log(spawn.name);
+
   var numberOfMiners = _.sum(creepsInRoom, (c) => c.memory.role == 'miner');
-  //if(debug == true){console.log('miner: ' + numberOfMiners);}
-  //console.log(numberOfMiners);
+
   var numberOfMovers = _.sum(creepsInRoom, (c) => c.memory.role == 'mover');
-//  if(debug == true){console.log('mover: ' + numberOfMovers);}
 
   var numberOfUpgradercons = _.sum(creepsInRoom, (c) => c.memory.role == 'upgradercon');
-//  if(debug == true){console.log('upgraders: ' + numberOfUpgradercons);}
 
   var numberOfUpgraders = _.sum(creepsInRoom, (c) => c.memory.role == 'upgrader');
-//  if(debug == true){console.log('upgraders: ' + numberOfUpgraders);}
 
   var numberOfRepairers = _.sum(creepsInRoom, (c) => c.memory.role == 'repairer');
-//  if(debug == true){console.log('repairers: ' + numberOfRepairers);}
 
   var numberOfBuilders = _.sum(creepsInRoom, (c) => c.memory.role == 'builder');
-//  if(debug == true){console.log('builders: ' + numberOfBuilders);}
 
   var numberOfWallRepairers = _.sum(creepsInRoom, (c) => c.memory.role == 'wallrepairer');
-//  if(debug == true){console.log('wall repairers: ' + numberOfWallRepairers);}
 
   var numberOfFillers = _.sum(creepsInRoom, (c) => c.memory.role == 'filler');
-//  if(debug == true){console.log('wall repairers: ' + numberOfWallRepairers);}
 
   var minimumNumberOfLongDistanceHarvestersT = 4;
   var numberOfLongDistanceHarvestersT = _.sum(Game.creeps, (c) => c.memory.role == 'longDistanceHarvester');
-//  if(debug == true){console.log('Long Distance Harvesters total: ' + numberOfLongDistanceHarvestersT);}
-  /*if(debug){
-    console.log(spawn.name + '    ' + numberOfHarvesters + '  ' + numberOfMiners + '  ' + numberOfMovers
-  + '  ' + numberOfUpgradercons+'  ' + numberOfRepairers + '  ' + numberOfBuilders
-+ '  ' + numberOfWallRepairers + '  ' + numberOfFillers + '  ' + numberOfLongDistanceHarvestersT)
-  }*/
   var maximumNumberofDefault = 0  ;
-  //spawn creeps
-  //if(numberOfHarvesters < minimumNumberOfHarvesters  || numberOfLongDistanceHarvestersT <3 && numberOfHarvesters < 4){
-  //  name = Game.spawns.Spawn1.createCustomCreep(energy, 'harvester');
-  //}
-
   var sources = spawn.room.find(FIND_SOURCES);
   for(let x in sources)
   {
@@ -199,28 +181,8 @@ if(tower) {
   else if(numberOfWallRepairers < spawn.memory.minimumNumberOfWallRepairers){
     name = spawn.createCustomCreep(energy, 'wallrepairer');
   }
-
-//
   else if(spawn.memory.sourceIDList != undefined || spawn.memory.sourceIDList != null) {
-  /*else if(numberOfLongDistanceHarvesters < minimumNumberOfLongDistanceHarvesters){
-    name = Game.spawns.Spawn1.createCustomCreeps(2,6,6, 'longDistanceHarvester',home,
-    '7cf80773019b1f1');
-  }
-  else if(numberOfLongDistanceHarvesters2 < minimumNumberOfLongDistanceHarvesters2){
-      name = Game.spawns.Spawn1.createCustomCreeps(2,6,6, 'longDistanceHarvester',home,
-      '70100773019e434');
-  }
-  else if(numberOfLongDistanceHarvesters3 < minimumNumberOfLongDistanceHarvesters3){
-        name = Game.spawns.Spawn1.createCustomCreeps(2,6,6, 'longDistanceHarvester',home,
-        '80d207728e6597b');
-}*/
-
-//long distance harvester spawning
-//var sourceIDlist = ['7cf80773019b1f1', '70100773019e434', '80d207728e6597b', 'c44207728e621fc','26f20772347f879'];
-//sourceIDlist.push('71ac0772347ffe6');
-//var targetList = ['W6N3','W6N3','W7N4','W7N4','W8N3','W8N3']
 //Get all of the spawns target list for long range harvesting
-
 var longRangeTargets = spawn.memory.sourceIDList
 
 for(let x in longRangeTargets){
@@ -235,26 +197,10 @@ for(let x in longRangeTargets){
 }
 
 }
+//if a creep was spawned let's log it
 if(!(name < 0) && name != undefined){
 console.log("Spawned new: " + name +'(' + Game.creeps[name].memory.role + ') at' + n );
 }
-}
-/*
-var sourceIDlist = ['7cf80773019b1f1', '70100773019e434', '80d207728e6597b', 'c44207728e621fc','26f20772347f879'];
-sourceIDlist.push('71ac0772347ffe6');
-var targetList = ['W6N3','W6N3','W7N4','W7N4','W8N3','W8N3']
-for(var i = 0;i < sourceIDlist.length; i++)
-{
-  if(_.sum(Game.creeps, (c) => c.memory.sourcetarget == sourceIDlist[i]) < 2)
-  {
-
-    var target = targetList[i];
-    name = Game.spawns.Spawn1.createCustomCreeps(3,7,7, 'longDistanceHarvester',target,home,
-    sourceIDlist[i]);
-
-
-    break;
-  }
 }
 */
 })
